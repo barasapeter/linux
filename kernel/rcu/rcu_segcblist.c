233ea889e2fef381c74379ef8893caf259bea078
barasa <101450156+barasapeter@users.noreply.github.com>
2024-08-14 18:09:54 +0300

    Update rcu_segcblist.c

--- kernel/rcu/rcu_segcblist.c
+++ kernel/rcu/rcu_segcblist.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * RCU segmented callback lists, function definitions
+ * RCU segmented  callback lists, function definitions
  *
  * Copyright IBM Corporation, 2017
  *
